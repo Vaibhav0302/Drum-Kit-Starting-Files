44db198fa885714fa1edc1d926ceb0c431bff9ec
Vaibhav0302 <vadeshmukh7744@gmail.com>
2023-03-02 15:27:21 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
-var numberOfDrum=document.querySelectorAll(".drum").length;
-var i=0;
-while(i<numberOfDrum)
-{
-    document.querySelectorAll("button")[i].addEventListener("click", function handleClicked(){
-        var audio= new Audio('sounds/tom-1.mp3');
-        audio.play();
+var numberOfDrum = document.querySelectorAll(".drum").length;
+var i = 0;
+
+//Detecting button press
+
+while (i < numberOfDrum) {
+    document.querySelectorAll("button")[i].addEventListener("click", function handleClicked() {
+        var buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);
     });
     i++;
+}
+
+//Detecting keyboard press
+
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        case "j":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "k":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+        case "l":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        default: console.log(buttonInnerHtml);
+            break;
+    }
 }
 
 
